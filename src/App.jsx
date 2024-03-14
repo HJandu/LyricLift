8ff5a86613e2c7dc70602e15e208e44b1b36c998
Hardip Kaur Jandu <116304118+HJandu@users.noreply.github.com>
2024-03-14 22:13:24 +0000

    Youtube api calls to search page. Installed tailwind for styling (#1)
    Co-authored-by: Hardip Jandu <hardip_jandu.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home'
 import Search from './pages/search/search'
 import Profile from './pages/profile/profile'
--- src/pages/search/search.jsx
+++ src/pages/search/search.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import ky from 'ky';
+
+const apiKey = 'AIzaSyC4BhWU5wZxX3oQx5ulJ9uQrixPIj6Xpoc'
+
+const fetchAPI = async () => {
+  const response = await ky(url).json();
+  console.log(response);
+}
 
 export default function Search() {
-    //when the user clicks on the search bar, the user will be taken to the search page
-
-
+    const [search, setSearch] = useState("");
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    }
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        console.log(search);
+        const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&type=video&key=${apiKey}`;
+        const response = await ky(url).json();
+        console.log(response);
+        // const spotifyURL= `https://api.spotify.com/v1/search?q=${search}&type=track`
+        // const spotifyResponse = await ky(spotifyURL).json();
+        // console.log(spotifyResponse);
+    }
     return(
         <div>
             <h1>Search page</h1>
+            <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Search for a song or artist" name="search" value={search} onChange={handleChange} />
+            <button type="submit">Search</button>
+            </form>
         </div>
+        // another div to output the search results
     )
 }
