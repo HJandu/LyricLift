f03f5627188fafea7eae3b57b2012b8b16176323
Hardip Jandu <hardip_jandu.com>
2024-03-14 22:00:16 +0000

    Youtube api calls to search page. Installed tailwind for styling

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -25,13 +25,13 @@ import { useNavigate } from 'react-router-dom';
 // };
 
 export default function Navbar() {
-    const [searchTerm, setSearchTerm] = useState('');
-    const history = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+  const history = useNavigate();
 
-    const handleSearch = () => {
-      // Redirect to the search page with the search term as a query parameter
-      history.push(`/search?q=${encodeURIComponent(searchTerm)}`);
-    };
+  const handleSearch = () => {
+    // Redirect to the search page with the search term as a query parameter
+    history.push(`/search?q=${encodeURIComponent(searchTerm)}`);
+  };
 
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -40,13 +40,13 @@ export default function Navbar() {
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <input 
-        className="form-control me-2" 
-        type="search" 
-        placeholder="Search" 
-        aria-label="Search" 
+      <input
+        className="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
         onChange={(e) => setSearchTerm(e.target.value)}
-        value={searchTerm}/>
+        value={searchTerm} />
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
