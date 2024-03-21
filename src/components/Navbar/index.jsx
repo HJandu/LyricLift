c49033a4e96f7cac492d77c817c3e683592486dc
D33ZY <138565452+ohhmydeezy@users.noreply.github.com>
2024-03-21 14:26:51 +0000

    Dg responsive-navbar (#14)
    * footer added to all pages
    
    * image-courousel added to home page as well as css for home page cleaned up
    
    * creating a responsive navbar and beginning to work on the overall responsiveness of the home page
    
    * responsive navbar made, working on making cards clickable
    
    ---------
    
    Co-authored-by: OhhMyDeezy <ohhmydeezy@MacDeezy.ultrahub>
    Co-authored-by: Hardip Kaur Jandu <116304118+HJandu@users.noreply.github.com>

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,34 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faMusic, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faMusic, faRightToBracket, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import 'materialize-css/dist/css/materialize.min.css';
 import "./navbar.css";
 
 export default function Navbar() {
-  return (
-    <nav>
-      <div className="nav-wrapper teal darken-3">
-        <Link to="/" className="brand-logo">
-          <img src="./src/assets/images/Logo.png" alt="Logo" width="50" height="50" className="rounded-logo" />
-        </Link>
 
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <Link to="/search">
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </Link>
-          </li>
-          <li>
-            <Link to="/main-song">
-              <FontAwesomeIcon icon={faMusic} />
-            </Link>
-          </li>
-          <li>
-            <Link to="/Login" ><FontAwesomeIcon icon={faRightToBracket} /></Link>
-          </li>
+  useEffect(() => {
+    // Initialize the sidenav
+    const sidenav = document.querySelector('.sidenav');
+    M.Sidenav.init(sidenav);
+  }, []);
+
+  return (
+    <>
+      <nav className="nav-wrapper teal darken-3">
+        <div className="container">
+          <Link to="/" className="brand-logo">
+            <img src="./src/assets/images/Logo.png" alt="Logo" className="rounded-logo" width={50} />
+          </Link>
+          <a href="#" data-target="mobile-links" className="sidenav-trigger">
+            <FontAwesomeIcon icon={faBars} />
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <Link to="/search">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </Link>
+            </li>
+            <li>
+              <Link to="/main-song">
+                <FontAwesomeIcon icon={faMusic} />
+              </Link>
+            </li>
+            <li>
+              <Link to="/Login" ><FontAwesomeIcon icon={faRightToBracket} /></Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="sidenav-container">
+        <ul className="sidenav" id="mobile-links">
+          <li><a href='/'>Home</a></li>
+          <li><a href='/search'>Search</a></li>
+          <li><a href='/main-song'>Main Song</a></li>
+          <li><a href='/Login'>Login</a></li>
+          <div className="close-btn">
+            <a href="#" className="close-icon" onClick={() => M.Sidenav.getInstance(document.querySelector('.sidenav')).close()}>
+              <FontAwesomeIcon icon={faTimes} />
+            </a>
+          </div>
         </ul>
+
       </div>
-    </nav>
+    </>
   );
 }
