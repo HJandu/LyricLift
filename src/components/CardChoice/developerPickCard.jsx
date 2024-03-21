71afbc84cfe5a158f5bd3f084feefed6e018b28c
ohhmydeezy <dylang21@hotmail.co.uk>
2024-03-21 17:56:39 +0000

    working on the clickable cards

--- src/components/CardChoice/developerPickCard.jsx
+++ src/components/CardChoice/developerPickCard.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./styleCard.css";
-import searchLogic from "../../utils/SearchFunction/searchFunction";
-//created single card component for developer pick card
+import SearchLogic from "../../utils/SearchFunction/searchFunction";
 
 export default function DeveloperPickCard({ name, title, image, description }) {
+  const handleTitleClick = () => {
+    console.log("Title clicked:", title);
+    return <SearchLogic search={title} />;
+  };
   return (
     <div className="card">
       <img src={image} className="card-img-top" alt={description} />
       <div className="card-body">
-        <h5 className="card-title" onClick={searchLogic}>
+        <h5 className="card-title">
           {name} {title}
         </h5>
       </div>
     </div>
-  )
+  );
 }
